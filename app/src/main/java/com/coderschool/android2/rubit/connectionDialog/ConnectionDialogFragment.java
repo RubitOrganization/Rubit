462bb57b9c26eca0166a1f6940c86cc9adf85891
TienVNguyen <tien.workinfo@gmail.com>
2016-11-04 23:49:19 +0700

    [Functions] Check Connection - Mistake for Copyright

--- app/src/main/java/com/coderschool/android2/rubit/connectionDialog/ConnectionDialogFragment.java
+++ app/src/main/java/com/coderschool/android2/rubit/connectionDialog/ConnectionDialogFragment.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2016. Self Training Systems, Inc - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * Written by TienVNguyen <tien.workinfo@gmail.com - tien.workinfo@icloud.com>, October 2015
+ * Written by <tien.workinfo@gmail.com - rubit1359@gmail.com - manetivinay@gmail.com>, October 2016
  */
 
 package com.coderschool.android2.rubit.connectionDialog;
